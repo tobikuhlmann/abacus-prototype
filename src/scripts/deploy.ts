9179554ff920ab6159ffb79d93c460c1c348cb5b
Tobias Kuhlmann <tobias.kuhlmann@clabs.co>
2022-07-11 13:12:19 +0200

    Initial commit

---
+++ src/scripts/deploy.ts
@@ -0,0 +1,32 @@
+import {
+  AbacusCore,
+  getMultiProviderFromConfigAndSigner,
+  serializeContracts,
+} from '@abacus-network/sdk';
+import '@nomiclabs/hardhat-ethers';
+import { ethers } from 'hardhat';
+import { getConfigMap, testConfigs } from '../deploy/config';
+import { HelloWorldDeployer } from '../deploy/deploy';
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+  const multiProvider = getMultiProviderFromConfigAndSigner(
+    testConfigs,
+    signer,
+  );
+
+  const core = AbacusCore.fromEnvironment('test', multiProvider);
+  const config = core.extendWithConnectionClientConfig(
+    getConfigMap(signer.address),
+  );
+
+  const deployer = new HelloWorldDeployer(multiProvider, config, core);
+  const chainToContracts = await deployer.deploy();
+  const addresses = serializeContracts(chainToContracts);
+  console.info('===Contract Addresses===');
+  console.info(JSON.stringify(addresses));
+}
+
+main()
+  .then(() => console.info('Deploy complete'))
+  .catch(console.error);
